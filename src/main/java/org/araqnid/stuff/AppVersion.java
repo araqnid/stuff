440456eaa0051609181f96dcda47226b8e2ecb18
Steve Haslam <araqnid@googlemail.com>
2014-08-12 00:58:11 +0100

    Expose application title and version to Javascript on embedded page

---
+++ src/main/java/org/araqnid/stuff/AppVersion.java
@@ -0,0 +1,13 @@
+package org.araqnid.stuff;
+
+public class AppVersion {
+	public final String version;
+	public final String title;
+	public final String vendor;
+
+	public AppVersion(String title, String vendor, String version) {
+		this.title = title;
+		this.vendor = vendor;
+		this.version = version;
+	}
+}
