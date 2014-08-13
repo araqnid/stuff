85bf28d7fdfb057a98ae2577fb999ce8d8e3ffb6
Steve Haslam <araqnid@googlemail.com>
2014-08-13 13:26:55 +0100

    No need for Exception wrapping in Main now Jetty startup is not there

--- src/main/java/org/araqnid/stuff/Main.java
+++ src/main/java/org/araqnid/stuff/Main.java
@@ -1,30 +1,21 @@
 package org.araqnid.stuff;
 
 import org.araqnid.stuff.config.AppConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
 
 public class Main {
-	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-
 	public static void main(String[] args) {
 		final Injector injector = Guice.createInjector(Stage.PRODUCTION, new AppConfig());
 		final AppServicesManager servicesManager = injector.getInstance(AppServicesManager.class);
-		try {
-			servicesManager.start();
-			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-				@Override
-				public void run() {
-					servicesManager.stop();
-				}
-			}, "GracefulShutdownThread"));
-		} catch (Exception e) {
-			LOG.error("Exception starting app", e);
-			System.exit(1);
-		}
+		servicesManager.start();
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				servicesManager.stop();
+			}
+		}, "GracefulShutdownThread"));
 	}
 }
