79dd272a1a422a133f85605a11fb2f98fd8148ad
Steve Haslam <araqnid@googlemail.com>
2014-08-13 13:04:32 +0100

    Take Jetty out of Main, just start it as an AppService

--- src/main/java/org/araqnid/stuff/Main.java
+++ src/main/java/org/araqnid/stuff/Main.java
@@ -1,7 +1,6 @@
 package org.araqnid.stuff;
 
 import org.araqnid.stuff.config.AppConfig;
-import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		final Injector injector = Guice.createInjector(Stage.PRODUCTION, new AppConfig());
 		final AppServicesManager servicesManager = injector.getInstance(AppServicesManager.class);
-		final Server server = injector.getInstance(Server.class);
 		try {
-			LOG.info("Starting web server");
-			server.start();
 			LOG.info("Starting app services");
 			servicesManager.start();
 			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
@@ -26,19 +22,10 @@ public class Main {
 				public void run() {
 					LOG.info("Starting graceful shutdown");
 					servicesManager.stop();
-					LOG.info("Stopping web server");
-					try {
-						server.stop();
-					} catch (Exception e) {
-						LOG.warn("Ignored exception shutting down web server: " + e);
-					}
 				}
 			}, "GracefulShutdownThread"));
-			LOG.info("Joining web server");
-			server.join();
-			System.exit(0);
 		} catch (Exception e) {
-			LOG.error("Exception starting web server", e);
+			LOG.error("Exception starting app", e);
 			System.exit(1);
 		}
 	}
