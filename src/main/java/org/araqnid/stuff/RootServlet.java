1fe47483c5c5ce7308f79108bd5355f5f6073a1f
Steve Haslam <araqnid@googlemail.com>
2014-08-17 00:57:38 +0100

    Forward requests for root of context to index.html

--- src/main/java/org/araqnid/stuff/RootServlet.java
+++ src/main/java/org/araqnid/stuff/RootServlet.java
@@ -7,26 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.araqnid.stuff.activity.RequestActivity;
-
-import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 @Singleton
 public class RootServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private final Provider<RequestActivity> stateProvider;
-
-	@Inject
-	public RootServlet(Provider<RequestActivity> stateProvider) {
-		this.stateProvider = stateProvider;
-	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("This is a test");
-		resp.getWriter().println("<!-- " + stateProvider.get().getRuid() + "-->");
+		req.getServletContext().getRequestDispatcher("/index.html").forward(req, resp);
 	}
 }
