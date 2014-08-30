bedf9f91102718398aaefe681388301f6fc0e810
Steve Haslam <araqnid@googlemail.com>
2014-08-30 20:46:19 +0100

    Inline the Beanstalk and WorkQueue bindings and wrap the Beanstalk processors in ServiceActivator instances

--- src/main/java/org/araqnid/stuff/config/RawBeanstalkModule.java
+++ src/main/java/org/araqnid/stuff/config/RawBeanstalkModule.java
@@ -1,18 +1,75 @@
 package org.araqnid.stuff.config;
 
-import org.araqnid.stuff.SometubeHandler;
+import java.lang.annotation.Annotation;
+import java.util.Collection;
+import java.util.Set;
 
+import org.araqnid.stuff.BeanstalkProcessor;
+import org.araqnid.stuff.BeanstalkProcessor.DeliveryTarget;
+import org.araqnid.stuff.ServiceActivator;
+import org.araqnid.stuff.SometubeHandler;
+import org.araqnid.stuff.activity.ActivityScopeControl;
+
+import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.Service;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Key;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Names;
+import com.google.inject.spi.Dependency;
+import com.google.inject.spi.ProviderWithDependencies;
 import com.surftools.BeanstalkClient.Client;
 import com.surftools.BeanstalkClientImpl.ClientImpl;
 
-public final class RawBeanstalkModule extends BeanstalkModule {
+public final class RawBeanstalkModule extends AbstractModule {
+	private final Collection<TubeConfiguration> configurations = ImmutableSet.of(new TubeConfiguration("sometube", 1,
+			SometubeHandler.class));
+
 	@Override
-	protected void configureDelivery() {
-		into(Multibinder.newSetBinder(binder(), Service.class));
-		process("sometube").with(SometubeHandler.class);
+	protected void configure() {
+		Multibinder<Service> services = Multibinder.newSetBinder(binder(), Service.class);
+
+		for (final TubeConfiguration tube : configurations) {
+			bind(Key.get(BeanstalkProcessor.class, tube.bindingAnnotation)).toProvider(
+					new ProviderWithDependencies<BeanstalkProcessor>() {
+						@Inject
+						private Provider<Client> connectionProvider;
+						@Inject
+						private ActivityScopeControl scopeControl;
+						private final Provider<? extends DeliveryTarget> targetProvider = binder().getProvider(
+								tube.processorClass);
+
+						@Override
+						public Set<Dependency<?>> getDependencies() {
+							return ImmutableSet.<Dependency<?>> of(Dependency.get(Key.get(Client.class)),
+									Dependency.get(Key.get(tube.processorClass)));
+						}
+
+						@Override
+						public BeanstalkProcessor get() {
+							return new BeanstalkProcessor(connectionProvider, tube.name, tube.threads, scopeControl,
+									targetProvider);
+						}
+					});
+			services.addBinding().toProvider(new ProviderWithDependencies<ServiceActivator<BeanstalkProcessor>>() {
+				private final Provider<BeanstalkProcessor> provider = binder().getProvider(
+						Key.get(BeanstalkProcessor.class, tube.bindingAnnotation));
+
+				@Override
+				public Set<Dependency<?>> getDependencies() {
+					return ImmutableSet.<Dependency<?>> of(Dependency.get(Key.get(BeanstalkProcessor.class,
+							tube.bindingAnnotation)));
+				}
+
+				@Override
+				public ServiceActivator<BeanstalkProcessor> get() {
+					return new ServiceActivator<BeanstalkProcessor>(provider, true);
+				}
+			});
+		}
 	}
 
 	@Provides
@@ -20,5 +77,19 @@ public final class RawBeanstalkModule extends BeanstalkModule {
 		ClientImpl client = new ClientImpl();
 		client.setUniqueConnectionPerThread(false);
 		return client;
+	}
+
+	private static class TubeConfiguration {
+		public final String name;
+		public final int threads;
+		public final Class<? extends DeliveryTarget> processorClass;
+		public final Annotation bindingAnnotation;
+
+		public TubeConfiguration(String name, int threads, Class<? extends DeliveryTarget> processorClass) {
+			this.name = name;
+			this.threads = threads;
+			this.processorClass = processorClass;
+			this.bindingAnnotation = Names.named(name);
+		}
 	}
 }
--- src/main/java/org/araqnid/stuff/config/WorkQueueModule.java
+++ src/main/java/org/araqnid/stuff/config/WorkQueueModule.java
@@ -1,32 +1,108 @@
 package org.araqnid.stuff.config;
 
+import java.lang.annotation.Annotation;
+import java.util.Collection;
+import java.util.Set;
+
+import org.araqnid.stuff.BeanstalkProcessor;
+import org.araqnid.stuff.ServiceActivator;
 import org.araqnid.stuff.SomeQueueProcessor;
+import org.araqnid.stuff.activity.ActivityScopeControl;
 import org.araqnid.stuff.activity.RequestActivity;
 import org.araqnid.stuff.workqueue.SqlWorkQueue;
-import org.araqnid.stuff.workqueue.WorkQueue;
+import org.araqnid.stuff.workqueue.WorkDispatcher;
+import org.araqnid.stuff.workqueue.WorkProcessor;
+import org.araqnid.stuff.workqueue.WorkQueueBeanstalkHandler;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.Service;
-import com.google.inject.Provides;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Key;
+import com.google.inject.Provider;
 import com.google.inject.multibindings.Multibinder;
-import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+import com.google.inject.spi.Dependency;
+import com.google.inject.spi.ProviderWithDependencies;
+import com.surftools.BeanstalkClient.Client;
 
-public final class WorkQueueModule extends BeanstalkWorkQueueModule {
+public final class WorkQueueModule extends AbstractModule {
+	private final Collection<QueueConfiguration> configurations = ImmutableSet.of(new QueueConfiguration("somequeue",
+			1, SomeQueueProcessor.class), new QueueConfiguration("otherqueue", 1, SomeQueueProcessor.class));
+
 	@Override
-	protected void configureDelivery() {
-		into(Multibinder.newSetBinder(binder(), Service.class));
-		process("somequeue").with(SomeQueueProcessor.class);
-		process("otherqueue").with(SomeQueueProcessor.class);
+	protected void configure() {
+		Multibinder<Service> services = Multibinder.newSetBinder(binder(), Service.class);
+
+		for (final QueueConfiguration queue : configurations) {
+			bind(Key.get(WorkQueueBeanstalkHandler.class, queue.bindingAnnotation)).toProvider(
+					new ProviderWithDependencies<WorkQueueBeanstalkHandler>() {
+						@Inject
+						private Provider<RequestActivity> requestActivityProvider;
+						private Provider<? extends WorkProcessor> processorProvider = binder().getProvider(
+								queue.processorClass);
+
+						@Override
+						public Set<Dependency<?>> getDependencies() {
+							return ImmutableSet.<Dependency<?>> of(Dependency.get(Key.get(RequestActivity.class)),
+									Dependency.get(Key.get(queue.processorClass)));
+						}
+
+						@Override
+						public WorkQueueBeanstalkHandler get() {
+							RequestActivity requestActivity = requestActivityProvider.get();
+							SqlWorkQueue queueImpl = new SqlWorkQueue(queue.name, requestActivity);
+							WorkDispatcher dispatcher = new WorkDispatcher(queueImpl, processorProvider.get());
+							WorkQueueBeanstalkHandler beanstalkTarget = new WorkQueueBeanstalkHandler(queue.name,
+									dispatcher, requestActivity);
+							return beanstalkTarget;
+						}
+					});
+			bind(Key.get(BeanstalkProcessor.class, queue.bindingAnnotation)).toProvider(
+					new ProviderWithDependencies<BeanstalkProcessor>() {
+						@Inject
+						private Provider<Client> connectionProvider;
+						@Inject
+						private ActivityScopeControl scopeControl;
+						private Provider<WorkQueueBeanstalkHandler> targetProvider = binder().getProvider(
+								Key.get(WorkQueueBeanstalkHandler.class, queue.bindingAnnotation));
+
+						@Override
+						public Set<Dependency<?>> getDependencies() {
+							return ImmutableSet.<Dependency<?>> of(Dependency.get(Key.get(Client.class)),
+									Dependency.get(Key.get(ActivityScopeControl.class)),
+									Dependency.get(Key.get(WorkQueueBeanstalkHandler.class, queue.bindingAnnotation)));
+						}
+
+						@Override
+						public BeanstalkProcessor get() {
+							return new BeanstalkProcessor(connectionProvider, queue.name, queue.threads, scopeControl,
+									targetProvider);
+						}
+					});
+			services.addBinding().toProvider(new Provider<ServiceActivator<BeanstalkProcessor>>() {
+				private Provider<BeanstalkProcessor> provider = binder().getProvider(
+						Key.get(BeanstalkProcessor.class, queue.bindingAnnotation));
+
+				@Override
+				public ServiceActivator<BeanstalkProcessor> get() {
+					return new ServiceActivator<BeanstalkProcessor>(provider, true);
+				}
+			});
+		}
 	}
 
-	@Provides
-	@Named("somequeue")
-	public WorkQueue somequeue(RequestActivity requestActivity) {
-		return new SqlWorkQueue("somequeue", requestActivity);
-	}
+	private static class QueueConfiguration {
+		public final String name;
+		public final int threads;
+		public final Class<? extends WorkProcessor> processorClass;
+		public final Annotation bindingAnnotation;
 
-	@Provides
-	@Named("otherqueue")
-	public WorkQueue otherqueue(RequestActivity requestActivity) {
-		return new SqlWorkQueue("otherqueue", requestActivity);
+		public QueueConfiguration(String name, int threads, Class<? extends WorkProcessor> processorClass) {
+			this.name = name;
+			this.threads = threads;
+			this.processorClass = processorClass;
+			this.bindingAnnotation = Names.named(name);
+		}
 	}
 }
