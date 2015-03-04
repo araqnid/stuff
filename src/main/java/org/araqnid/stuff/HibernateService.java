4294b28ba301a8925c768d40559816140be5175c
Steve Haslam <araqnid@googlemail.com>
2015-03-04 21:23:00 +0000

    Restore Hibernate configuation to use data source

--- src/main/java/org/araqnid/stuff/HibernateService.java
+++ src/main/java/org/araqnid/stuff/HibernateService.java
@@ -1,26 +1,46 @@
 package org.araqnid.stuff;
 
+import javax.sql.DataSource;
+
 import org.araqnid.stuff.services.ProviderService;
+import org.araqnid.stuff.workqueue.WorkItem;
+import org.araqnid.stuff.workqueue.WorkItemEvent;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 
 public class HibernateService extends ProviderService<SessionFactory> {
+	private final DataSource dataSource;
+
 	@Inject
-	public HibernateService() {
+	public HibernateService(DataSource dataSource) {
 		super(SessionFactory.class);
+		this.dataSource = dataSource;
 	}
 
 	private SessionFactory sessionFactory;
 
 	@Override
 	protected void startUp() throws Exception {
-		StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-		StandardServiceRegistry serviceRegistry = registryBuilder.build();
 		Configuration cfg = new Configuration();
+		cfg.addAnnotatedClass(WorkItem.class);
+		cfg.addAnnotatedClass(WorkItemEvent.class);
+		cfg.setNamingStrategy(new ImprovedNamingStrategy());
+		cfg.setProperty(Environment.USE_SQL_COMMENTS, "true");
+		cfg.setProperty("hibernate.hbm2ddl.auto", "update");
+		DatasourceConnectionProviderImpl connectionProvider = new DatasourceConnectionProviderImpl();
+		connectionProvider.setDataSource(dataSource);
+		connectionProvider.configure(ImmutableMap.of());
+		StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(cfg.getProperties()).addService(ConnectionProvider.class, connectionProvider).build();
 		sessionFactory = cfg.buildSessionFactory(serviceRegistry);
 	}
 
