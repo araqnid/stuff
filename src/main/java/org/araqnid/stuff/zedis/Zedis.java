a399bf7e8d5bf91ec1600f082845eb21abfe6041
Steve Haslam <araqnid@googlemail.com>
2015-11-29 19:15:46 +0000

    Separate ZedisConnection to a top-level class

--- src/main/java/org/araqnid/stuff/zedis/Zedis.java
+++ src/main/java/org/araqnid/stuff/zedis/Zedis.java
@@ -4,12 +4,10 @@ import java.io.Closeable;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Deque;
 import java.util.Optional;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.CompletableFuture;
@@ -18,14 +16,11 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.eclipse.jetty.io.AbstractConnection;
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
 import org.eclipse.jetty.io.ManagedSelector;
 import org.eclipse.jetty.io.SelectChannelEndPoint;
 import org.eclipse.jetty.io.SelectorManager;
-import org.eclipse.jetty.util.Callback;
-import org.eclipse.jetty.util.IteratingCallback;
 import org.eclipse.jetty.util.thread.Scheduler;
 import org.eclipse.jetty.util.thread.TimerScheduler;
 import org.slf4j.Logger;
@@ -38,7 +33,6 @@ public class Zedis implements Closeable {
 	private final ZedisSelectorManager selectorManager;
 	private final InetSocketAddress address;
 	private final Scheduler scheduler;
-	private SocketChannel socket;
 	private final AtomicReference<ZedisConnection> readyConnection = new AtomicReference<>();
 	private final BlockingDeque<Command> pendingCommands = new LinkedBlockingDeque<>();
 
@@ -63,7 +57,7 @@ public class Zedis implements Closeable {
 	}
 
 	private void beginConnection() throws IOException {
-		socket = SocketChannel.open();
+		SocketChannel socket = SocketChannel.open();
 		socket.configureBlocking(false);
 		if (socket.connect(address)) {
 			LOG.debug("connected to {}", address);
@@ -82,10 +76,6 @@ public class Zedis implements Closeable {
 		if (connection != null) {
 			LOG.debug("Closing connection");
 			connection.close();
-		}
-		else if (socket != null) {
-			LOG.debug("Closing socket");
-			socket.close();
 		}
 		try {
 			selectorManager.stop();
@@ -176,190 +166,20 @@ public class Zedis implements Closeable {
 		public void connectionOpened(Connection connection) {
 			super.connectionOpened(connection);
 			Zedis.LOG.info("connected to {}", address);
+			connectionReady((ZedisConnection) connection);
 		}
 
 		@Override
 		public void connectionClosed(Connection connection) {
 			super.connectionClosed(connection);
 			Zedis.LOG.info("connection closed: {}", connection);
+			connectionInvalid((ZedisConnection) connection);
 		}
 
 		@Override
 		protected void connectionFailed(SocketChannel channel, Throwable ex, Object attachment) {
 			super.connectionFailed(channel, ex, attachment);
 			Zedis.LOG.error("connection failed", ex);
-		}
-	}
-
-	enum SendState {
-		CONNECTING, IDLE, SENDING, BLOCKING
-	};
-
-	enum ReceiveState {
-		CONNECTING, IDLE, LISTENING, READING
-	};
-
-	public class ZedisConnection extends AbstractConnection {
-		private SendState sendState = SendState.CONNECTING;
-		private ReceiveState recvState = ReceiveState.CONNECTING;
-		private final Deque<Command> commandsSent = new LinkedBlockingDeque<>();
-		private final Deque<Command> commandQueue = new LinkedBlockingDeque<>();
-		private final ByteBuffer responseBuf = ByteBuffer.allocate(1024);
-
-		public ZedisConnection(EndPoint endp, Executor executor) {
-			super(endp, executor);
-			responseBuf.position(0);
-			responseBuf.limit(0);
-		}
-
-		public final class SendCommand extends IteratingCallback {
-			private final Command command;
-			private final Callback callback;
-			private final ByteBuffer msg;
-
-			public SendCommand(Command command, Callback callback) {
-				this.command = command;
-				this.callback = callback;
-				msg = ByteBuffer.wrap(command.asBytes());
-			}
-
-			@Override
-			protected Action process() throws Exception {
-				if (msg.position() == msg.limit()) {
-					if (command.isBlocking()) {
-						LOG.debug("finished flushing command -> BLOCKING");
-						sendState = SendState.BLOCKING;
-					}
-					else {
-						LOG.debug("finished flushing command -> IDLE");
-						sendState = SendState.IDLE;
-					}
-					callback.succeeded();
-					startListening();
-					return Action.SUCCEEDED;
-				}
-				LOG.debug("sending {}", command);
-				getEndPoint().write(this, msg);
-				return Action.SCHEDULED;
-			}
-		}
-
-		public final class QueueSender extends IteratingCallback {
-			@Override
-			protected Action process() throws Exception {
-				Command command;
-				synchronized (ZedisConnection.this) {
-					LOG.debug("QueueSender: sendState={} recvState={} qsize={}", sendState, recvState,
-							commandQueue.size());
-					if (sendState != SendState.IDLE) return Action.SUCCEEDED;
-					command = commandQueue.poll();
-					if (command == null) return Action.SUCCEEDED;
-					sendState = SendState.SENDING;
-					commandsSent.addLast(command);
-				}
-				new SendCommand(command, this).iterate();
-				return Action.SCHEDULED;
-			}
-		}
-
-		private void startListening() {
-			boolean start;
-			synchronized (this) {
-				if (recvState == ReceiveState.IDLE) {
-					start = true;
-					recvState = ReceiveState.LISTENING;
-				}
-				else {
-					start = false;
-				}
-			}
-			if (start) fillInterested();
-		}
-
-		@Override
-		public void onOpen() {
-			super.onOpen();
-			synchronized (this) {
-				sendState = SendState.IDLE;
-				recvState = ReceiveState.IDLE;
-			}
-			connectionReady(this);
-		}
-
-		public void send(Command command) {
-			commandQueue.add(command);
-			sendCommands();
-		}
-
-		public void sendCommands() {
-			new QueueSender().iterate();
-		}
-
-		@Override
-		public void onFillable() {
-			synchronized (this) {
-				recvState = ReceiveState.READING;
-			}
-			try {
-				int got = getEndPoint().fill(responseBuf);
-				LOG.debug("filled {}", got);
-
-				if (got < 0) {
-					LOG.warn("EOF on input");
-					connectionInvalid(this);
-					socket.close();
-					return;
-				}
-
-				boolean doMore = true;
-				while (doMore && responseBuf.position() < responseBuf.limit()) {
-					Command sentCommand = commandsSent.getFirst();
-					LOG.debug("responding to {} ({})", sentCommand, responseBuf);
-					if (sentCommand.received(responseBuf)) {
-						commandsSent.removeFirst();
-						if (sentCommand.isBlocking()) {
-							LOG.debug("got response to blocking command, reactivate send queue");
-							sendState = SendState.IDLE;
-							new QueueSender().iterate();
-						}
-						doMore = true;
-					}
-					else {
-						doMore = false;
-					}
-				}
-			} catch (IOException e) {
-				LOG.error("Exception filling input", e);
-			}
-			boolean listen;
-			synchronized (this) {
-				if (!commandsSent.isEmpty()) {
-					recvState = ReceiveState.LISTENING;
-					listen = true;
-				}
-				else {
-					recvState = ReceiveState.IDLE;
-					listen = false;
-				}
-			}
-			if (listen) fillInterested();
-		}
-
-		@Override
-		public void onClose() {
-			super.onClose();
-			LOG.debug("onClose {}", this);
-			for (Command command : commandsSent) {
-				command.failed(new EOFException());
-			}
-			for (Command command : commandQueue) {
-				command.failed(new EOFException());
-			}
-		}
-
-		@Override
-		public String toString() {
-			return "ZedisConnection{" + sendState + "," + recvState + "}";
 		}
 	}
 }
---
+++ src/main/java/org/araqnid/stuff/zedis/ZedisConnection.java
@@ -0,0 +1,185 @@
+package org.araqnid.stuff.zedis;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Deque;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.eclipse.jetty.io.AbstractConnection;
+import org.eclipse.jetty.io.EndPoint;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.IteratingCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ZedisConnection extends AbstractConnection {
+	private static final Logger LOG = LoggerFactory.getLogger(ZedisConnection.class);
+	private ZedisConnection.SendState sendState = ZedisConnection.SendState.CONNECTING;
+	private ZedisConnection.ReceiveState recvState = ZedisConnection.ReceiveState.CONNECTING;
+	private final Deque<Command> commandsSent = new LinkedBlockingDeque<>();
+	private final Deque<Command> commandQueue = new LinkedBlockingDeque<>();
+	private final ByteBuffer responseBuf = ByteBuffer.allocate(1024);
+
+	public ZedisConnection(EndPoint endp, Executor executor) {
+		super(endp, executor);
+		responseBuf.position(0);
+		responseBuf.limit(0);
+	}
+
+	public final class SendCommand extends IteratingCallback {
+		private final Command command;
+		private final Callback callback;
+		private final ByteBuffer msg;
+
+		public SendCommand(Command command, Callback callback) {
+			this.command = command;
+			this.callback = callback;
+			msg = ByteBuffer.wrap(command.asBytes());
+		}
+
+		@Override
+		protected Action process() throws Exception {
+			if (msg.position() == msg.limit()) {
+				if (command.isBlocking()) {
+					LOG.debug("finished flushing command -> BLOCKING");
+					sendState = ZedisConnection.SendState.BLOCKING;
+				}
+				else {
+					LOG.debug("finished flushing command -> IDLE");
+					sendState = ZedisConnection.SendState.IDLE;
+				}
+				callback.succeeded();
+				startListening();
+				return Action.SUCCEEDED;
+			}
+			LOG.debug("sending {}", command);
+			getEndPoint().write(this, msg);
+			return Action.SCHEDULED;
+		}
+	}
+
+	public final class QueueSender extends IteratingCallback {
+		@Override
+		protected Action process() throws Exception {
+			Command command;
+			synchronized (ZedisConnection.this) {
+				LOG.debug("QueueSender: sendState={} recvState={} qsize={}", sendState, recvState, commandQueue.size());
+				if (sendState != ZedisConnection.SendState.IDLE) return Action.SUCCEEDED;
+				command = commandQueue.poll();
+				if (command == null) return Action.SUCCEEDED;
+				sendState = ZedisConnection.SendState.SENDING;
+				commandsSent.addLast(command);
+			}
+			new SendCommand(command, this).iterate();
+			return Action.SCHEDULED;
+		}
+	}
+
+	private enum SendState {
+		CONNECTING, IDLE, SENDING, BLOCKING
+	}
+
+	private enum ReceiveState {
+		CONNECTING, IDLE, LISTENING, READING
+	}
+
+	private void startListening() {
+		boolean start;
+		synchronized (this) {
+			if (recvState == ZedisConnection.ReceiveState.IDLE) {
+				start = true;
+				recvState = ZedisConnection.ReceiveState.LISTENING;
+			}
+			else {
+				start = false;
+			}
+		}
+		if (start) fillInterested();
+	}
+
+	@Override
+	public void onOpen() {
+		synchronized (this) {
+			sendState = ZedisConnection.SendState.IDLE;
+			recvState = ZedisConnection.ReceiveState.IDLE;
+		}
+		super.onOpen();
+	}
+
+	public void send(Command command) {
+		commandQueue.add(command);
+		sendCommands();
+	}
+
+	public void sendCommands() {
+		new QueueSender().iterate();
+	}
+
+	@Override
+	public void onFillable() {
+		synchronized (this) {
+			recvState = ZedisConnection.ReceiveState.READING;
+		}
+		try {
+			int got = getEndPoint().fill(responseBuf);
+			LOG.debug("filled {}", got);
+
+			if (got < 0) {
+				LOG.warn("EOF on input");
+				getEndPoint().close();
+				return;
+			}
+
+			boolean doMore = true;
+			while (doMore && responseBuf.position() < responseBuf.limit()) {
+				Command sentCommand = commandsSent.getFirst();
+				LOG.debug("responding to {} ({})", sentCommand, responseBuf);
+				if (sentCommand.received(responseBuf)) {
+					commandsSent.removeFirst();
+					if (sentCommand.isBlocking()) {
+						LOG.debug("got response to blocking command, reactivate send queue");
+						sendState = ZedisConnection.SendState.IDLE;
+						new QueueSender().iterate();
+					}
+					doMore = true;
+				}
+				else {
+					doMore = false;
+				}
+			}
+		} catch (IOException e) {
+			LOG.error("Exception filling input", e);
+		}
+		boolean listen;
+		synchronized (this) {
+			if (!commandsSent.isEmpty()) {
+				recvState = ZedisConnection.ReceiveState.LISTENING;
+				listen = true;
+			}
+			else {
+				recvState = ZedisConnection.ReceiveState.IDLE;
+				listen = false;
+			}
+		}
+		if (listen) fillInterested();
+	}
+
+	@Override
+	public void onClose() {
+		super.onClose();
+		LOG.debug("onClose {}", this);
+		for (Command command : commandsSent) {
+			command.failed(new EOFException());
+		}
+		for (Command command : commandQueue) {
+			command.failed(new EOFException());
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "ZedisConnection{" + sendState + "," + recvState + "}";
+	}
+}
