6a38c61397a50a707bb933dde600d96a29638976
Steve Haslam <araqnid@googlemail.com>
2015-11-29 21:12:32 +0000

    Refactor RedisProcessor to be event-driven rather than having a main thread

--- src/main/java/org/araqnid/stuff/messages/RedisProcessor.java
+++ src/main/java/org/araqnid/stuff/messages/RedisProcessor.java
@@ -1,7 +1,11 @@
 package org.araqnid.stuff.messages;
 
 import java.io.EOFException;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.araqnid.stuff.zedis.ZedisClient;
 import org.araqnid.stuff.zedis.ZedisConnection;
@@ -10,16 +14,18 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.MDC.MDCCloseable;
 
-import com.google.common.util.concurrent.AbstractExecutionThreadService;
+import com.google.common.util.concurrent.AbstractService;
 import com.google.common.util.concurrent.Monitor;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.CompletableFuture.allOf;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
-public class RedisProcessor extends AbstractExecutionThreadService {
+public class RedisProcessor extends AbstractService {
 	private final ZedisClient client;
 	private final String key;
-	private final String processingSuffix = ".working";
+	private final String inProgressKey;
 	private final DeliveryTarget target;
 	private final Logger log;
 	private final Monitor monitor = new Monitor();
@@ -31,79 +37,42 @@ public class RedisProcessor extends AbstractExecutionThreadService {
 	};
 	private ZedisConnection conn;
 	private boolean delivering;
+	private final ExecutorService deliveryExecutor;
 
 	public RedisProcessor(ZedisClient client, String key, DeliveryTarget target) {
 		this.client = client;
 		this.key = key;
 		this.target = target;
 		this.log = LoggerFactory.getLogger(RedisProcessor.class.getName() + "." + key);
+		this.deliveryExecutor = Executors
+				.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("RedisProcessor-" + key + "-%d").build());
+		this.inProgressKey = key + ".working";
 	}
 
 	@Override
-	protected void startUp() throws Exception {
-		conn = client.connect().join();
-	}
-
-	@Override
-	protected void run() throws Exception {
-		String inProgressKey = key + processingSuffix;
-		log.info("Consuming from Redis list \"{}\"", key);
-		while (isRunning()) {
-			log.debug("retrieving value from list");
-			String value;
-			try {
-				value = conn.brpoplpush(key, inProgressKey, 30).thenApply(RedisProcessor::asString).join();
-			} catch (CompletionException e) {
-				if (!isRunning() && e.getCause() instanceof EOFException) {
-					log.debug("Ignoring network exception during shutdown: " + e);
-					return;
-				}
-				throw e;
-			}
-			if (value != null) {
-				monitor.enter();
-				try {
-					if (!isRunning()) {
-						log.error("<{}> aborting delivery due to shutdown", value);
-						allOf(conn.rpush(key, value), conn.lrem(inProgressKey, 0, value)).join();
-						return;
-					}
-					log.debug("<{}> retrieved", value);
-					delivering = true;
-				} finally {
-					monitor.leave();
-				}
-				try {
-					if (deliver(value)) {
-						conn.lrem(inProgressKey, 0, value).join();
-						log.debug("<{}> removed", value);
-					}
-					else {
-						allOf(conn.lrem(inProgressKey, 0, value), conn.lpush(key, value)).join();
-						log.debug("<{}> recycled", value);
-					}
-				} catch (Exception e) {
-					log.error("Unhandled exception delivering message, leaving it on in-progress key", e);
-				}
-				monitor.enter();
-				try {
-					delivering = false;
-				} finally {
-					monitor.leave();
-				}
-			}
+	protected void doStart() {
+		CompletableFuture<ZedisConnection> connectionFuture;
+		try {
+			connectionFuture = client.connect();
+		} catch (IOException e) {
+			notifyFailed(e);
+			return;
 		}
-		log.debug("exiting main loop");
+		connectionFuture.whenComplete((ZedisConnection newConnection, Throwable ex) -> {
+			if (newConnection != null) {
+				conn = newConnection;
+				notifyStarted();
+				log.info("Consuming from Redis list \"{}\"", key);
+				poll();
+			}
+			else {
+				notifyFailed(ex);
+			}
+		});
 	}
 
 	@Override
-	protected void shutDown() throws Exception {
-		log.info("Consumption stopped");
-		conn.close();
-	}
-
-	@Override
-	protected void triggerShutdown() {
+	protected void doStop() {
 		if (monitor.enterIf(notCurrentlyDelivering)) {
 			log.debug("closing connection to trigger shutdown");
 			monitor.leave();
@@ -114,9 +83,84 @@ public class RedisProcessor extends AbstractExecutionThreadService {
 		}
 	}
 
-	@Override
-	protected String serviceName() {
-		return "RedisProcessor-" + key;
+	private void poll() {
+		conn.brpoplpush(key, inProgressKey, 30).thenApply(RedisProcessor::asString).thenAccept(value -> {
+			if (value == null) {
+				poll();
+			}
+			else {
+				processValue(value);
+			}
+		}).whenComplete((x, e) -> {
+			if (e != null) {
+				if (!isRunning() && e instanceof CompletionException && e.getCause() instanceof EOFException) {
+					log.debug("Ignoring network exception during shutdown: " + e);
+					notifyStopped();
+				}
+				else {
+					notifyFailed(e);
+				}
+			}
+		});
+	}
+
+	private void processValue(String value) {
+		monitor.enter();
+		boolean abort = false;
+		try {
+			if (!isRunning()) {
+				abort = true;
+			}
+			else {
+				delivering = true;
+			}
+		} finally {
+			monitor.leave();
+		}
+
+		if (abort) {
+			log.error("<{}> aborting delivery due to shutdown", value);
+			allOf(conn.rpush(key, value), conn.lrem(inProgressKey, 0, value)).whenComplete((result, ex2) -> {
+				conn.close();
+				notifyStopped();
+			});
+			return;
+		}
+		else {
+			dispatch(value);
+		}
+	}
+
+	private void dispatch(String value) {
+		supplyAsync(() -> deliver(value), deliveryExecutor).thenCompose(delivered -> {
+			if (delivered) {
+				return conn.lrem(inProgressKey, 0, value).thenRun(() -> {
+					log.debug("<{}> removed", value);
+				});
+			}
+			else {
+				return allOf(conn.lrem(inProgressKey, 0, value), conn.lpush(key, value)).thenRun(() -> {
+					log.debug("<{}> recycled", value);
+				});
+			}
+		}).whenComplete((x, err) -> {
+			if (err != null) {
+				log.error("Unhandled exception delivering message, leaving it on in-progress key", err);
+			}
+			monitor.enter();
+			try {
+				delivering = false;
+			} finally {
+				monitor.leave();
+			}
+			if (isRunning()) {
+				poll();
+			}
+			else {
+				conn.close();
+				notifyStopped();
+			}
+		});
 	}
 
 	private boolean deliver(String value) {
